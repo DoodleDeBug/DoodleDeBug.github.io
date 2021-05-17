f25a6ce83942fb37348220735faed830aa85c3c3
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-05-17 12:22:40 +0100

    make nav fixed again

---
+++ index.js
@@ -0,0 +1,12 @@
+// links closed
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  linksContainer.classList.toggle("show-links");
+});
+
+//fived navbar
+
+window.addEventListener("scroll", function () {});
